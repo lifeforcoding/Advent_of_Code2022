e2de7fb476c4f2ae3c17c4d7e0d9f91d7f0ee29b
Chandan Maharana <85704460+lifeforcoding@users.noreply.github.com>
2022-12-04 01:30:10 +0530

    Added Part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -17,3 +17,19 @@ fun process(key:Char) : Int {
         (key.code - 'a'.code) + 1
     else (key.code - 'A'.code) + 27
 }
+
+
+//Part 2
+fun main() {
+    var i = 0
+    while (i < menuData.size) {
+        val set1: Set<Char> = menuData[i++].toMutableList().toMutableSet()
+    val set2: Set<Char> = menuData[i++].toMutableList().toMutableSet()
+        val set3: Set<Char> = menuData[i++].toMutableList().toMutableSet()
+    set3.forEach { data ->
+        if (set1.contains(data) && set2.contains(data))
+            ans += process(data)
+    }
+}
+    println(ans)
+}
